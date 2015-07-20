3ef24f04c258cdaf3433bf7b85d87c2af3a14868
Marc Prewitt <marc@chartbeat.com>
2015-07-20 12:07:20 -0400

    [asana] add direct to board page for /

--- asana/urls.py
+++ asana/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    url(r'^$', RedirectView.as_view(url='/static/boards/index.html')),
     url(r'^boards/', include('boards.urls')),
 )
